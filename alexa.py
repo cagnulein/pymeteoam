6c7803377cdeea7be3f60d8845fd8aa6b7d8fdbc
Roberto Viola <cagnulein@gmail.com>
2019-12-18 08:32:36 +0100

    in caso di citta non trovata, provo tra tutte le parole dette per alexa

--- alexa.py
+++ alexa.py
@@ -1,8 +1,25 @@
 import sys
 from meteoam import *
 
+found = False
+
 try:
    w = MeteoAM(sys.argv[1])
    print(w.alexa_today())
+   found = True
 except:
+   if(len(sys.argv[1].split())==1):
+     found = False
+   else:
+     for a in sys.argv[1].split():
+       try:
+         w = MeteoAM(a)
+         print(w.alexa_today())
+         found = True
+         break
+       except:
+         found = False
+
+if(found == False):
    print("Non ho trovato la citta' " + sys.argv[1] + ". Mi spiace.")
+
